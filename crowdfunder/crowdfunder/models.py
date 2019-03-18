938aab449aaed33b579fc8ff6222adfcbc447aae
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-18 11:34:19 -0400

    updated the model to remove the number of remaining rewards and allow the total rewards to be null

--- crowdfunder/crowdfunder/models.py
+++ crowdfunder/crowdfunder/models.py
@@ -15,8 +15,7 @@ class RewardTier(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     tier_value = models.IntegerField()
-    total_rewards = models.IntegerField()
-    remaining_rewards = models.IntegerField()
+    total_rewards = models.IntegerField(null=True, blank=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='reward_tiers')
 
 
