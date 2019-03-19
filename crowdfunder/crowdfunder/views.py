1473d4ef4cb93ec10eaa354e26692bce7e784be7
christinegithub <christine13@gmail.com>
2019-03-19 16:00:36 -0400

    added to project page to show deadline met and funding goal met

--- crowdfunder/crowdfunder/views.py
+++ crowdfunder/crowdfunder/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 from crowdfunder.models import Project, RewardTier, Purchase
 from crowdfunder.forms import RewardTierForm, PurchaseForm, ProjectForm, LoginForm
 import ipdb
+import datetime
 
 def home_page(request):
     context = {'projects': Project.objects.all() }
@@ -16,7 +17,15 @@ def home_page(request):
 def project_page(request, id):
     project = Project.objects.get(pk=id)
     owner = project.owner
-    context = {'project': project, "owner":owner}
+    # Determine deadline
+    today = datetime.date.today()
+    d0 = today
+    d1 = project.end_date
+    delta = d1 - d0
+    days_left = delta.days
+    # Determine funds received
+    funds_met = project.total_pledged() - project.funding_goal
+    context = {'project': project, "owner":owner, 'today': today, 'days_left': days_left, 'funds_met': funds_met}
     response = render(request, 'project.html', context)
     return HttpResponse(response)
 
